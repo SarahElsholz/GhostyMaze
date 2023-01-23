4e3473f88219afc6600c04fc9011d4c84e76c981
Sarah Elsholz <elsholz@mailbox.org>
2023-01-23 20:02:58 +0100

    Updates

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div class="p-8 flex justify-center">
+    <div class="pt-10 flex justify-center">
+        <a href="/maze" class="flex flex-col items-center border-2 dark:border-middleblue border-middleblue rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/src/assets/ghostMaze3.png" alt="ghost">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Welcome to Ghosty's Super Maze</h5>
+                <p class="mb-3 font-normal text-gray-400">Check out our little maze game and navigate the little Ghost through the corridors.</p>
+                <p class="mb-3 font-normal text-gray-400">To navigate Ghosty, use the ⬆️➡️⬇️⬅️ keys.</p>
+                <p class="mb-3 font-normal text-gray-400">You can use the "Restart"-Button under the maze to start from level one again.</p>
+            </div>
+        </a>
+    </div>
+</div>
+
+<div class="p-5 flex justify-center">
+    <div class="flex flex-col items-center rounded-lg shadow-md md:flex-row md:max-w-xl border-2 dark:border-middleblue border-middleblue bg-sky-50 hover:bg-gray-700">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Collect fruits and presents</h5>
+            <p class="mb-3 font-normal text-gray-400">Every 5 seconds new fruits and presents are generated and the old ones disappear.</p>
+            <p class="mb-3 font-normal text-gray-400">Collect as many fruits as you can and as many presents as you want. But be aware that one of the presents is a <strong class="text-darkred dark:text-brightred">FRAUD</strong>. It will immediately end your game.</p>
+            <p class="mb-3 font-normal text-gray-400">Each fruit will raise your score by 1 while each "good" present will raise it by 10.</p>
+        </div>
+        <img class="md:h-auto md:w-1/2 pr-4" src="/src/assets/collectables.png" alt="fruit+present">
+    </div>
+</div>
+
+<div class="p-5 flex justify-center">
+    <div class="flex flex-col items-center rounded-lg shadow-md md:flex-row md:max-w-xl border-2 dark:border-middleblue border-middleblue  bg-sky-50 hover:bg-gray-700">
+        <img class="object-cover rounded-t-lg ml-4 md:h-auto md:w-28 md:rounded-none md:rounded-l-lg" src="/src/assets/clock.png" alt="clock">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Be aware of the time</h5>
+            <p class="mb-3 font-normal text-gray-400">The clock is ticking.....try to help Ghosty escape the maze in as fast as you can.</p>
+        </div>
+    </div>
+</div>
